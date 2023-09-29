84b794c0f0cd1648b775648470094ae15a6352d8
Iamneo <techadmin@iamneo.ai>
2023-09-29 17:09:04 +0000

    Backup Commit

---
+++ TrySolution/CustomerCQRSProject/dotnetproject/dotnetmicroserviceone/Program.cs
@@ -0,0 +1,28 @@
+using dotnetmicroserviceone.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<CustomerDbContext>(options =>{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnString"));
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
